a32b029e426491eb4b01e51d56965c315c07919b
satyapsr13 <satyapsr13@gmail.com>
2022-11-03 22:26:05 +0530

    initial commit

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -8,10 +8,7 @@ function Footer() {
           <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
               <h5 className="text-uppercase">Footer Content</h5>
-              <p>
-                Here you can use rows and columns to organize your footer
-                content.
-              </p>
+              
             </div>
             <hr className="clearfix w-100 d-md-none pb-3" />
             <div className="col-md-3 mb-md-0 mb-3">
