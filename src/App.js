d298d4d07d6345ec29ae44720879aa00b3b161fd
satyapsr13 <satyapsr13@gmail.com>
2022-11-05 20:21:34 +0530

    Redux setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Header/navbar";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <figcaption>Add your list here</figcaption>
+            <div className="addItems">
+              <input type="text" placeholder="✍ Add Items" />
+              <i className="fa fa-plus add-btn"></i>
+            </div>
+          </figure>
+        </div>
+      </div>
+    </>
   );
 }
 
